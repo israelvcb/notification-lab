ada51d1d356fcf69f8ad8b94de6e1e192a4a09a4
ISRAEL FRANÇA <israelvcb@hotmail.com>
2025-04-14 11:23:34 -0300

    feat: adicionar lógica para cancelar, contar e buscar notificações por recipientId no NotificationController

--- src/infra/http/controllers/notification/notification.controller.ts
+++ src/infra/http/controllers/notification/notification.controller.ts
@@ -1,17 +1,73 @@
+import { CancelNotification } from '@app/use-cases/notification/cancel-notification.use-case';
+import { CountRecipientNotification } from '@app/use-cases/notification/count-recipient-notification.use-case';
+import { GetRecipientNotification } from '@app/use-cases/notification/get-recipient-notification.use-case';
+import { ReadNotification } from '@app/use-cases/notification/read-notification.use-case';
 import { SendNotification } from '@app/use-cases/notification/send-notification.use-case';
+import { UnReadNotification } from '@app/use-cases/notification/unread-notification.use-case';
 import { NotificationViewModel } from '@infra/http/view-models/notification.view-model';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateNotificationBodyDto } from '../../dtos/notification/create-notification-body.dto';
 
 @Controller('notification')
 export class NotificationController {
-  constructor(private sendNotificationService: SendNotification) {}
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification,
+    private readNotification: ReadNotification,
+    private unReadNotification: UnReadNotification,
+    private countFromRecipientId: CountRecipientNotification,
+    private getFromRecipientId: GetRecipientNotification,
+  ) {}
+
+  @Patch('cancel/:notificationId')
+  async cancel(@Param('notificationId') notificationId: string) {
+    await this.cancelNotification.execute({
+      notificationId,
+    });
+  }
+
+  @Get('count/from-recipientId/:recipientId')
+  async countFromRecipient(@Param('recipientId') recipientId: string) {
+    const { count } = await this.countFromRecipientId.execute({
+      recipientId,
+    });
+    return {
+      count,
+    };
+  }
+
+  @Get('from-recipientId/:recipientId')
+  async getFromRecipient(@Param('recipientId') recipientId: string) {
+    const { notifications } = await this.getFromRecipientId.execute({
+      recipientId,
+    });
+
+    return {
+      notifications: notifications.map((notification) =>
+        NotificationViewModel.toHTTP(notification),
+      ),
+    };
+  }
+
+  @Patch('read/:notificationId')
+  async read(@Param('notificationId') notificationId: string) {
+    await this.readNotification.execute({
+      notificationId,
+    });
+  }
+
+  @Patch('unread/:notificationId')
+  async unread(@Param('notificationId') notificationId: string) {
+    await this.unReadNotification.execute({
+      notificationId,
+    });
+  }
 
   @Post()
   async create(@Body() body: CreateNotificationBodyDto) {
     const { category, content, recipientId } = body;
 
-    const { notification } = await this.sendNotificationService.execute({
+    const { notification } = await this.sendNotification.execute({
       category,
       content,
       recipientId,
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,4 +1,9 @@
+import { CancelNotification } from '@app/use-cases/notification/cancel-notification.use-case';
+import { CountRecipientNotification } from '@app/use-cases/notification/count-recipient-notification.use-case';
+import { GetRecipientNotification } from '@app/use-cases/notification/get-recipient-notification.use-case';
+import { ReadNotification } from '@app/use-cases/notification/read-notification.use-case';
 import { SendNotification } from '@app/use-cases/notification/send-notification.use-case';
+import { UnReadNotification } from '@app/use-cases/notification/unread-notification.use-case';
 import { DataBaseModule } from '@infra/database/database.module';
 import { Module } from '@nestjs/common';
 import { NotificationController } from './controllers/notification/notification.controller';
@@ -8,10 +13,11 @@ import { NotificationController } from './controllers/notification/notification.
   controllers: [NotificationController],
   providers: [
     SendNotification,
-    // {
-    //   provide: 'NotificationRepository',
-    //   useClass: PrismaNotificationRepository,
-    // },
+    GetRecipientNotification,
+    CancelNotification,
+    UnReadNotification,
+    ReadNotification,
+    CountRecipientNotification,
   ],
 })
 export class HttpModule {}
